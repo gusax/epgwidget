85a4999e0a5197a1fc62390140428815d6b06a30
gustav.axelsson <gustav.axelsson@0b6e9397-262f-0410-bd52-dbfbf314b74d>
2008-09-06 17:07:18 +0000

    Added a proper version number to updateInfo

--- trunk/updateInfo.js
+++ trunk/updateInfo.js
@@ -3,7 +3,7 @@
   {
     "stable" :
     {
-      "version" : 20081006,
+      "version" : 20080826,
       "blogUrl" : "http://epgwidget.blogspot.com/2008/08/epg-version-20080826.html",
       "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20080826.zip", 
       "message" : "New version out! Get it while it's hot!", 
