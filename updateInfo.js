ec2f650022559ec014c7f22a84f90c7ea6c57a4c
gustav.axelsson <gustav.axelsson@0b6e9397-262f-0410-bd52-dbfbf314b74d>
2008-09-06 14:56:05 +0000

    Added blogUrl to updateInfo

--- updateInfo.js
+++ updateInfo.js
@@ -4,7 +4,7 @@
     "stable" :
     {
       "version" : 20081006,
-      "blogUrl" : 
+      "blogUrl" : "http://epgwidget.blogspot.com",
       "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20080826.zip", 
       "message" : "New version out! Get it while it's hot!", 
       "mustUpgrade" : false
