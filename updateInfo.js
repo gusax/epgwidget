9184c981f264684b73a51bc97bf0608b1f375a9c
gustav.axelsson <gustav.axelsson@0b6e9397-262f-0410-bd52-dbfbf314b74d>
2008-09-15 18:00:42 +0000

    Updated updateInfo.js

--- updateInfo.js
+++ updateInfo.js
@@ -3,9 +3,9 @@
   {
     "stable" :
     {
-      "version" : 20080826,
-      "blogUrl" : "http://epgwidget.blogspot.com/2008/08/epg-version-20080826.html",
-      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20080826.zip", 
+      "version" : 20080915,
+      "blogUrl" : "http://epgwidget.blogspot.com/2008/09/epg-version-20080915-tigervnlig.html",
+      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20080915.zip", 
       "message" : "New version out! Get it while it's hot!", 
       "mustUpgrade" : false
     }
