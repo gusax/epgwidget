d3b28e89776e73e1bd3761dbfc23d22026f28abc
gustav.axelsson <gustav.axelsson@0b6e9397-262f-0410-bd52-dbfbf314b74d>
2008-12-07 09:13:35 +0000

    Update updateInfo.js to enable automatic updates

--- trunk/updateInfo.js
+++ trunk/updateInfo.js
@@ -3,9 +3,9 @@
   {
     "stable" :
     {
-      "version" : 20080915,
-      "blogUrl" : "http://epgwidget.blogspot.com/2008/09/epg-version-20080915-tigervnlig.html",
-      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20080915.zip", 
+      "version" : 20081207,
+      "blogUrl" : "http://epgwidget.blogspot.com/2008/12/epg-version-20081207.html",
+      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20081207.zip", 
       "message" : "New version out! Get it while it's hot!", 
       "mustUpgrade" : false
     }
