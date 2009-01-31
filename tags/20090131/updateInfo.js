23a3b913ab318d553044413b243bc9dff177d7e1
gustav.axelsson <gustav.axelsson@0b6e9397-262f-0410-bd52-dbfbf314b74d>
2009-01-31 10:08:56 +0000

    20090131

---
+++ tags/20090131/updateInfo.js
@@ -0,0 +1,13 @@
+{
+  "updateInfo" : 
+  {
+    "stable" :
+    {
+      "version" : 20081207,
+      "blogUrl" : "http://epgwidget.blogspot.com/2008/12/epg-version-20081207.html",
+      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20081207.zip", 
+      "message" : "New version out! Get it while it's hot!", 
+      "mustUpgrade" : false
+    }
+  } 
+}
