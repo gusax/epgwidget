e69433666ab5a028df129f46bb421941e56e7122
gustav.axelsson <gustav.axelsson@0b6e9397-262f-0410-bd52-dbfbf314b74d>
2009-05-31 09:58:36 +0000

    Incremented version information in updateInfo.js

--- updateInfo.js
+++ updateInfo.js
@@ -3,9 +3,9 @@
   {
     "stable" :
     {
-      "version" : 20090131,
-      "blogUrl" : "http://epgwidget.blogspot.com/2009/01/epg-version-20090131.html",
-      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20090131.zip", 
+      "version" : 20090531,
+      "blogUrl" : "http://epgwidget.blogspot.com/2009/05/epg-version-20090531.html",
+      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20090531.zip", 
       "message" : "New version out! Get it while it's hot!", 
       "mustUpgrade" : false
     }
