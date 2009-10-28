6143651a8d0fde243539bf0e07fb49f55542f2a8
gustav.axelsson <gustav.axelsson@0b6e9397-262f-0410-bd52-dbfbf314b74d>
2009-10-28 19:35:45 +0000

    updateInfo.js: Stepped version

--- trunk/updateInfo.js
+++ trunk/updateInfo.js
@@ -3,9 +3,9 @@
   {
     "stable" :
     {
-      "version" : 20090921,
-      "blogUrl" : "http://epgwidget.blogspot.com/2009/09/epg-version-20090921.html",
-      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20090921.zip", 
+      "version" : 20091028,
+      "blogUrl" : "http://epgwidget.blogspot.com/2009/10/epg-version-20091028.html",
+      "fileUrl" : "http://epgwidget.googlecode.com/files/EPG20091028.zip", 
       "message" : "New version out! Get it while it's hot!", 
       "mustUpgrade" : false
     }
